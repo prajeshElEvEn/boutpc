4ba304ba70a21abfc7040af716af6a02d5e8eadc
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-02-25 11:04:14 +0530

    - updated to 1.0.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const os = require("os")
 
-const boutpc = () => {
+// const boutpc = () => {
     console.log("------------------------")
     console.log("boutPC - Know You PC")
     console.log("------------------------")
@@ -12,16 +12,19 @@ const boutpc = () => {
     console.log("Platform - ", os.platform())
     console.log("OS - ", os.type())
     console.log("Release - ", os.release())
+    
 
     let tmem = Math.floor(os.totalmem() / (1024 * 1024))
-    console.log("Total Memory - ", tmem, "GBs")
+    console.log("Total Memory - ", tmem, "MBs")
     let fmem = Math.floor(os.freemem() / (1024 * 1024))
-    console.log("Free Memory - ", fmem, "GBs")
+    console.log("Free Memory - ", fmem, "MBs")
 
-    let hours = Math.floor(os.uptime() / 3600)
-    let min = Math.floor((os.uptime() % 3600) / 60)
-    console.log("Uptime - ", hours, "hrs", min, "min")
+    // let hours = (Math.floor(os.uptime() / 3600))%12
+    // let min = Math.floor((os.uptime() % 3600) / 60)
+    // console.log("Uptime - ", hours, "hrs", min, "min")
+    // console.log("CPU - ", os.cpus())
+    // console.log("Network Interface- ", os.networkInterfaces())
     console.log("------------------------")
-}
+// }
 
-module.exports = boutpc
+// module.exports = boutpc
